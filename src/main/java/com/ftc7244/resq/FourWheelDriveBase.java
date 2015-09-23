52b7c882cdeba088e046b8e4b6fbcf1a80cb26f3
brandon <brandonbarker767@gmail.com>
2015-09-23 01:57:10 -0400

    Convert to maven

--- src/main/java/com/ftc7244/resq/FourWheelDriveBase.java
+++ src/main/java/com/ftc7244/resq/FourWheelDriveBase.java
@@ -2,10 +2,22 @@ package com.ftc7244.resq;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
- * Created by Iam on 9/14/2015.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Written By: Out of the Box on 9/14/2015
  */
 public abstract class FourWheelDriveBase extends OpMode {
 
---
+++ src/main/java/com/ftc7244/resq/FourWheelSimpleDrive.java
@@ -0,0 +1,34 @@
+package com.ftc7244.resq;
+
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Written By: Out of the Box on 9/14/2015
+ */
+public class FourWheelSimpleDrive extends FourWheelDriveBase {
+
+    public FourWheelSimpleDrive() {
+        super("motor1", "motor2", "motor3", "motor4");
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+        setPowerSides(getJoy1LeftY(), getJoy1RightY());
+    }
+}
